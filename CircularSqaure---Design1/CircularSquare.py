ae0e7ca2c31c7f77c419884d6ae163b771537e85
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-08 17:02:49 +0530

    code

--- CircularSqaure---Design1/CircularSquare.py
+++ CircularSqaure---Design1/CircularSquare.py
@@ -1,3 +1,18 @@
 import turtle
 
-akash = turtle.Turtle
+akash = turtle.Turtle()
+turtle.bgcolor("black")
+
+akash.speed(100)
+akash.pencolor('#ffffe6')
+akash.pensize(0.2)
+
+area = 0
+
+while area < 430:
+    akash.forward(2+area)
+    akash.right(90.899)
+
+    area = area + 1
+
+turtle.done()
